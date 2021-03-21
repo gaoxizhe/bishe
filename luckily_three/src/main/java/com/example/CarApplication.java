4e72561096d4479f6666d21e0fea122820a50628
gaoxizhe-ubuntu <a.gxz@qq.com>
2021-03-21 20:48:59 +0800

    1

--- luckily_three/src/main/java/com/example/CarApplication.java
+++ luckily_three/src/main/java/com/example/CarApplication.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @MapperScan("com.example.dao")
 @ComponentScan(basePackages = "com.example.*")
+
 public class CarApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarApplication.class, args);
